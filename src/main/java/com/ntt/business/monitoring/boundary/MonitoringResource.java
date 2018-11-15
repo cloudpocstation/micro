aae2cb7894b9533eb10213784f8abb53dda399aa
Alexander Burkard <alexander.burkard@nttdata.com>
2018-11-15 15:15:18 +0100

    add

--- src/main/java/com/ntt/business/monitoring/boundary/MonitoringResource.java
+++ src/main/java/com/ntt/business/monitoring/boundary/MonitoringResource.java
@@ -18,6 +18,6 @@ public class MonitoringResource {
     @Path("ping")
     public String ping(){
         // test comment
-        return "Alive awsome and running - " + System.currentTimeMillis();
+        return "Alive awsome and running asd- " + System.currentTimeMillis();
     }
 }
